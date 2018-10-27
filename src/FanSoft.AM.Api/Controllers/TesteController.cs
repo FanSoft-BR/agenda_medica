945f76e673cb8ce3e4fac07b8030b810029ae8ff
Fabiano Nalin <fabiano.nalin@gmail.com>
2018-10-27 20:53:06 -0300

    Subindo a solution cmo a api

---
+++ src/FanSoft.AM.Api/Controllers/TesteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FanSoft.AM.Api.Controllers
+{
+    public class TesteController
+    {
+
+        [HttpGet("api/v1/ping"), AllowAnonymous]
+        public string Ping() => "Pong";
+    }
+}
